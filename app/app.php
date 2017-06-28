c8bc3cf202c44f31aec2ff453fc09b1e288ba983
Dylan Lewis, Nathan Stewart and Steve Spitz <dylan@>
2017-06-28 09:28:46 -0700

    start session, start twig application and declare path

--- app/app.php
+++ app/app.php
@@ -3,9 +3,19 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Places.php";
 
+    session_start();
+
+    if(empty($_SESSION['list_of_cities'])) {
+        $_SESSION['list_of_cities'] = array();
+    }
+
     $app = new Silex\Application();
 
-    
+    $app->resister(new Silex\Provider\TwigServiceProvider(), array(
+        "twig.path" => __DIR__."/../views"
+    ));
+
+
 
 
 
